70ab62ec2ce8dc804eec5d043b6103808c82f7c6
2fand <167662034+2fand@users.noreply.github.com>
2024-09-26 17:02:43 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -740,3 +740,21 @@ int main() {
 	return 0;
 }
 *///“函数的默认占位参数”^
+/*
+class m_int {
+public:
+	m_int(int inum) {
+		i = inum;
+	}
+	int operator+(m_int mi){
+		return i + mi.i;
+	}
+	int i;
+};
+int main() {
+	m_int mi = 10;
+	m_int mia = 666;
+	cout << mi + mia << endl;
+	return 0;
+}
+*///“重载加号运算符”^
