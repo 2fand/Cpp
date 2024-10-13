41659a39829a78ac113512cfc9a0e02c098b7f0a
2fand <167662034+2fand@users.noreply.github.com>
2024-10-13 17:48:57 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -3227,3 +3227,31 @@ public:
 	void mosterdo();
 };
 *///所有怪物已完成构造^
+/*
+//M&.cpp
+#include <iostream>
+using namespace std;
+#include "m&.h"
+Mand::Mand() {
+	this->set();
+}
+//vector<trir>* = NULL, char** = NULL, char** = NULL, pair<int, int>* = NULL, int = 0, int = 0, int = 3, bool = 0, bool = 0
+void Mand::set(vector<trir>* v, char** cpp, int iheal, char**, char(*)[11][11], int, int, bool b, bool) {
+	m_v = v;
+	m_cpp = cpp;
+	m_iheal = iheal;
+	mblr = b;
+}
+void Mand::mosterdo() {
+	**m_cpp = ' ';
+	if (!mblr && '*' == (*m_cpp)[-1]) {
+		mblr = 1;
+	}
+	else if (mblr && '*' == (*m_cpp)[1]) {
+		mblr = 0;
+	}//撞墙换方向
+	(*m_cpp) -= (1 - 2 * mblr);//左右移
+	'*' != (*m_cpp)[11] && ((*m_cpp) += 11);//下落
+	**m_cpp = '&';//使见
+}//撞墙换方向，会自然下落
+*///“‘M&’正式死亡”^
