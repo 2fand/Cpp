f3e0c88fb8a02e27a4ed0c20605bdddc0025025e
2fand <167662034+2fand@users.noreply.github.com>
2024-10-18 20:34:02 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -6403,3 +6403,49 @@ int main() {
 	return 0;
 }
 *///“已无明显BUG”^
+/*
+//MX.cpp
+#include <iostream>
+#include <cstring>
+using namespace std;
+#include "mX.h"
+void MX::set(vector<trir>* v, char** cpp, int iheal, char** pcpp, char(*strmap)[11][11], int, int, bool, bool) {
+	m_v = v;//vector<trir>* = NULL, char** = NULL, char** = NULL, pair<int, int>* = NULL, char(*)[11][11] = NULL,int = 0, int = 0, int = 3, bool = 0, bool = 0
+	m_cpp = cpp;
+	m_pcpp = pcpp;
+	m_strmap = strmap;
+	m_iheal = iheal;
+	setb = 1;
+}
+void MX::exset(char strmapr[11][11]) {
+	memcpy(m_strmapr, strmapr, sizeof m_strmapr);
+}
+MX::MX() {
+	set();
+}
+void MX::mosterdo() {
+	vector<trir>::iterator it = m_v->begin();
+	if (setb) {
+		pxy = { (*m_cpp - &(*m_strmap)[0][0]) / 11, (*m_cpp - &(*m_strmap)[0][0]) % 11 };
+		m_cc = m_strmapr[pxy.first][setb = 0, pxy.second];
+	}
+	for (; m_v->end() != it; it++) {
+		if (*m_cpp == it->cp) {
+			break;
+		}
+	}
+	if (*m_cpp == *m_pcpp || m_v->end() != it) {
+		char* tempcp = *m_cpp;
+		do {
+			m_v->end() != it && (**m_cpp = ' ');
+			*m_cpp = &m_strmapr[pxy.first = rand() % 9 + 1][pxy.second = rand() % 9 + 1];
+		} while (*m_cpp == &(*m_strmap)[9][1] || '*' == (*m_cpp)[-1] && '*' == (*m_cpp)[1]);
+		*tempcp = m_cc;
+		'X' != m_strmapr[pxy.first][pxy.second] && (m_cc = m_strmapr[pxy.first][pxy.second]);
+	}
+	//**m_cpp = 'X';//使见
+}//被玩家碰到传，受伤传(两旁有砖不传)，并见
+char MX::getm() {
+	return 'X';
+}
+*///“‘MX’保留BUG已解决”^
