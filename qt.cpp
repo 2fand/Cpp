7e01fe357783043a7f2474487b982f1a429bea14
2fand <167662034+2fand@users.noreply.github.com>
2024-10-27 13:12:12 +0800

    Update qt.cpp

--- qt.cpp
+++ qt.cpp
@@ -12,3 +12,20 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 *///“第一个qt程序”^
+/*
+//main.cpp
+#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    Widget wa;
+    wa.show();
+
+    return a.exec();
+}
+*///“两个widget”^
