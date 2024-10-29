4938852a3686172a0c8365eaf649ee700b6c7d96
2fand <167662034+2fand@users.noreply.github.com>
2024-10-29 19:48:37 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -13724,4 +13724,86 @@ int main() {
 	for_each(vo.begin(), vo.end(), delvo);
 	return 0;
 }
-*///“M+”房已构造完成^
+*///“M+”房机制已构造完成^
+/*
+//MO.h
+#pragma once
+#include <iostream>
+using namespace std;
+#include <map>
+#include "Player.h"
+#include "moster.h"
+class MO : public moster {
+private:
+	WASD m_wasd;
+	map<char**, WASD>m_m;//[strmap point, wasd]...
+	pair<int, int> m_pxy;//MOSTER xy坐标
+	static pair<int, int> s_m_pxy;//PLAYER xy坐标
+	int imove;
+public:
+	void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* m = NULL, char** = NULL, char(*)[11][11] = NULL, int ix = 0, int iy = 0, bool = 0, bool = 0);
+	MO();
+	void set_s_pxy(int ix, int iy);
+	void mosterdo();
+	char getm();
+};
+//MO.cpp
+#include <iostream>
+using namespace std;
+#include "MO.h"
+#define NNEG(A, B) ((A) < (B) ? 1 : (A) > (B) ? -1 : 0)
+pair<int, int> MO::s_m_pxy = {0, 0};
+MO::MO() {
+	this->set();
+}
+void MO::set(vector<trir>* v, char** cpp, int iheal, map<char**, WASD>*m, char**, char(*)[11][11], int ix, int iy, bool, bool) {
+	m_v = v;//vector<trir>* = NULL, char** = NULL, pair<int, int>* = NULL, int = 0, int = 0, int = 3, bool = 0, bool = 0
+	m_cpp = cpp;
+	m_iheal = iheal;
+	m_pxy.first = ix;
+	m_pxy.second = iy;
+	imove = false;
+	if (m) { 
+		m_m = *m;
+	}
+}
+void MO::mosterdo() {
+	'*' != **m_cpp && (**m_cpp = ' ');
+	tempcp = *m_cpp;
+	m_cpp = NULL;
+	if (!m_m.empty()) {
+		int arr[4] = { -11, -1, 11, 1 };
+		for (map<char**, WASD>::iterator it = m_m.begin(); m_m.end() != it; it++) {
+			E != it->second && tempcp == *it->first && (m_wasd = it->second);
+		}
+		//int* iparr[2] = { &m_pxy.first, &m_pxy.second };
+		switch (tempcp += (arr[m_wasd]), m_wasd) {
+		case W:
+			m_pxy.second--;
+			break;
+		case A:
+			m_pxy.first--;
+			break;
+		case S:
+			m_pxy.second++;
+			break;
+		case D:
+			m_pxy.first++;
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		2 == imove % 3 && ('*' != *(tempcp + 11 * NNEG(m_pxy.first, s_m_pxy.first)) && (tempcp += 11 * NNEG(m_pxy.first, s_m_pxy.first), m_pxy.first += NNEG(m_pxy.first, s_m_pxy.first)));//根据怪物的xy坐标和玩家的xy坐标来移动
+		2 == imove++ % 3 && ('*' != *(tempcp + NNEG(m_pxy.second, s_m_pxy.second)) && (tempcp += NNEG(m_pxy.second, s_m_pxy.second), m_pxy.second += NNEG(m_pxy.second, s_m_pxy.second)));
+	}
+	m_cpp = &tempcp;
+}//遇向则转，看路前行（1），或追玩者（2）
+void MO::set_s_pxy(int ix, int iy) {
+	s_m_pxy = { ix, iy };
+}
+char MO::getm() {
+	return 'O';
+}
+*///“MO”的追踪机制已更改^
