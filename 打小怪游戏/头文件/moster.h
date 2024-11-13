23809a95ecd1cc098722b2d13b0d58210bf72d84
2fand <167662034+2fand@users.noreply.github.com>
2024-11-13 19:01:16 +0800

    Update moster.h

--- 打小怪游戏/头文件/moster.h
+++ 打小怪游戏/头文件/moster.h
@@ -6,16 +6,16 @@ using namespace std;
 #include <map>
 class moster {
 protected:
-	char** m_cpp;
-	int m_iheal;
-	vector<trir>* m_v;
-	char* tempcp;
+	char** m_cpp;//怪物的位置
+	int m_iheal;//怪物的血量数
+	vector<trir>* m_v;//指向玩家射出的子弹
+	char* tempcp;//用于在移动怪物的位置的时候防出bug
 public:
-	moster();
-	virtual void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* = NULL, char** = NULL, char(*)[11][11] = NULL, int = 0, int = 0, bool = 0, bool = 0);
-	void hunt();
-	int getheal();
-	virtual void mosterdo() = 0;
-	virtual char getm() = 0;
-	char*& getcp();
+	moster();//moster抽象基类的构造函数
+	virtual void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* = NULL, char** = NULL, char(*)[11][11] = NULL, int = 0, int = 0, bool = 0, bool = 0);//设置怪物的一些属性
+	void hunt();//受伤函数
+	int getheal();//获取怪物的血量数
+	virtual void mosterdo() = 0;//执行每种怪物要做的事
+	virtual char getm() = 0;//获取怪物的类型，用多态实现
+	char*& getcp();//获取怪物的位置
 };
