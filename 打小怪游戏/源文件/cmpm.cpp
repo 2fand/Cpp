987ec3531ba331c830537dea613a6a777b34009a
2fand <167662034+2fand@users.noreply.github.com>
2024-11-15 20:09:14 +0800

    Update cmpm.cpp

--- 打小怪游戏/源文件/cmpm.cpp
+++ 打小怪游戏/源文件/cmpm.cpp
@@ -3,5 +3,5 @@
 #include "moster.h"
 using namespace std;
 bool cmpm::operator()(moster* mp, moster* mpa) {
-	return mp->getheal() < mpa->getheal();
+	return mp->getheal() < mpa->getheal();//比较两个怪物的生命值
 }
