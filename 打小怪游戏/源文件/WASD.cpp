87b6e8e4e4c3ef20df0b31324ad41aa5dcfd5ac8
2fand <167662034+2fand@users.noreply.github.com>
2024-11-15 20:07:52 +0800

    Update WASD.cpp

--- 打小怪游戏/源文件/WASD.cpp
+++ 打小怪游戏/源文件/WASD.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #include "WASD.h"
 WASD& operator++(WASD& w) {
-	WASD warr[5] = { W,A,S,D,E };
-	w < E && (w = warr[w + 1]);
+	WASD warr[5] = { W,A,S,D,E };//把WASD枚举类型的每一种东西都转化成数字
+	w < E && (w = warr[w + 1]);//warr[w + 1](WASD) == w + 1(int)
 	return w;
 }
