a0d543b86bf09e47942c8b86a9f88ebb04241fc6
2fand <167662034+2fand@users.noreply.github.com>
2024-11-15 20:08:48 +0800

    Create Mplus.cpp

---
+++ 打小怪游戏/源文件/Mplus.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include "m+.h"
+#include "M_o.h"
+#include "Player.h"
+void Mplus::mosterdo() {
+	**m_cpp = '+';//后面根据**m_cpp的内容来执行summon方法
+}
+char Mplus::getm() {
+	return '+';
+}
