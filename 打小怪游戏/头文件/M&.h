c46e548f07ec05cd7e8db5e4cd9b561d650ff2aa
2fand <167662034+2fand@users.noreply.github.com>
2024-11-15 20:40:28 +0800

    Update M&.h

--- 打小怪游戏/头文件/M&.h
+++ 打小怪游戏/头文件/M&.h
@@ -4,10 +4,10 @@ using namespace std;
 #include "moster.h"
 class Mand: public moster {
 private:
-	bool mblr;
+	bool mblr;//朝向(0< 1>)
 public:
-	void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* = NULL, char** = NULL, char(*)[11][11] = NULL, int = 0, int = 0, bool b = 0, bool = 0);
-	Mand();
-	void mosterdo();
-	char getm();
+	void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* = NULL, char** = NULL, char(*)[11][11] = NULL, int = 0, int = 0, bool b = 0, bool = 0);//设置怪物M&的一些属性
+	Mand();//构造函数
+	void mosterdo();//执行怪物M&要做的事
+	char getm();//获取怪物M&的类型
 };
