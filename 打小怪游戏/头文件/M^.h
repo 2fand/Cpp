a926933b0fd1df8acfc70d5b7d926b91dc76b863
2fand <167662034+2fand@users.noreply.github.com>
2024-11-15 20:43:01 +0800

    Update M^.h

--- 打小怪游戏/头文件/M^.h
+++ 打小怪游戏/头文件/M^.h
@@ -4,11 +4,11 @@ using namespace std;
 #include "moster.h"
 class MUD : public moster {
 private:
-	bool mblr;
-	bool mbud;
+	bool mblr;//水平朝向(0< 1>)
+	bool mbud;//垂直朝向(0^ 1v)
 public:
-	MUD();
-	void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* = NULL, char** = NULL, char(*)[11][11] = NULL, int = 0, int = 0, bool b = 0, bool ba = 0);
-	void mosterdo();
-	char getm();
+	MUD();//构造函数
+	void set(vector<trir>* v = NULL, char** cpp = NULL, int iheal = 3, map<char**, WASD>* = NULL, char** = NULL, char(*)[11][11] = NULL, int = 0, int = 0, bool b = 0, bool ba = 0);//设置怪物M^的一些属性
+	void mosterdo();//执行怪物M^要做的事
+	char getm();//获取怪物M^的类型
 };
