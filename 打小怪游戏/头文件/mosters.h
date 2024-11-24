d7f05332f7b531fad30cb1711f153d8418f61258
2fand <167662034+2fand@users.noreply.github.com>
2024-11-24 15:29:21 +0800

    Update mosters.h

--- 打小怪游戏/头文件/mosters.h
+++ 打小怪游戏/头文件/mosters.h
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 #include "moster.h"//怪物们的头文件
-#include "m_o.h"
-#include "m&.h"
-#include "m^.h"
-#include "mO.h"
-#include "m+.h"
-#include "mX.h"
+#include "M_o.h"
+#include "M&.h"
+#include "M^.h"
+#include "MO.h"
+#include "M+.h"
+#include "MX.h"
