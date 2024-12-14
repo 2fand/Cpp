dd3a83bc0dad7bcbfc647f2799cdfe4c6468527b
2fand <167662034+2fand@users.noreply.github.com>
2024-12-14 15:53:42 +0800

    Update MX.cpp

--- 打小怪游戏/源文件/MX.cpp
+++ 打小怪游戏/源文件/MX.cpp
@@ -5,11 +5,19 @@ using namespace std;
 //怪物MX的设置函数
 void MX::set(vector<trir>* v, char** cpp, int iheal, map<char**, WASD>*, char** pcpp, char(*strmap)[11][11], int, int, bool, bool) {
 	m_v = v;
-	m_cpp = cpp;
+	if (nullptr == cpp) {
+		m_cpp = cpp;
+	}
+	else {
+		tempcp = *cpp;
+		cpp = NULL;
+		m_cpp = &tempcp;
+	}
 	m_pcpp = pcpp;
-	strmap && (m_strmap = strmap);
+	nullptr != strmap && (m_strmap = strmap);
 	m_iheal = iheal;
 	setb = 1;
+	
 }
 //怪物MX的额外设置函数
 void MX::exset(char strmapr[11][11]) {
@@ -30,7 +38,7 @@ void MX::mosterdo() {
 			break;
 		}
 	}
-	if (m_iheal && (*m_cpp == *m_pcpp || m_v->end() != it)) {//如果刚才的这个循环停止了，并且还有生命且不在玩家的位置上
+	if (m_iheal > 0 && (*m_cpp == *m_pcpp || m_v->end() != it)) {//如果刚才的这个循环停止了，并且还有生命且不在玩家的位置上
 		tempcp = *m_cpp;//防指针的链式移动
 		char* tempcpb = tempcp;
 		bool tempb = 1;//防误覆盖空位
@@ -38,8 +46,7 @@ void MX::mosterdo() {
 		do {
 			tempb && m_v->end() != it && (*tempcp = ' ');//便于移动之后的显示
 			tempcp = &(*m_strmap)[pxy.first = rand() % 9 + 1][pxy.second = rand() % 9 + 1];//怪物MX的移动
-
-		} while ((tempcp == &(*m_strmap)[9][1] || '@' == (*m_strmap)[pxy.first][pxy.second] || '@' == (*m_strmap)[pxy.first][pxy.second - 1] || '@' == (*m_strmap)[pxy.first][pxy.second + 1] || tempcp == *m_pcpp || '*' == tempcp[-1] || '*' == tempcp[1] || 'X' == (tempcp)[-1] || 'X' == (tempcp)[1]) && (tempb = 0, 1));//如果满足上述的条件，就把误覆盖空位的“开关”启动
+		} while ((tempcp == &(*m_strmap)[9][1] || '@' == (*m_strmap)[pxy.first][pxy.second] || '@' == (*m_strmap)[pxy.first][pxy.second - 1] || '@' == (*m_strmap)[pxy.first][pxy.second + 1] || tempcp == *m_pcpp || '*' == tempcp[-1] || '*' == tempcp[1] || 'X' == (tempcp)[-1] || 'X' == *tempcp || 'X' == (tempcp)[1]) && (tempb = 0, 1));//如果满足上述的条件，就把误覆盖空位的“开关”启动
 		m_cpp = &tempcp;
 		*tempcpb = m_cc;
 		('@' != m_strmapr[pxy.first][pxy.second] || 'X' != m_strmapr[pxy.first][pxy.second] && 'P' != m_strmapr[pxy.first][pxy.second]) && (m_cc = m_strmapr[pxy.first][pxy.second]);//记录盖住的块，不记录“@”子弹，“P”玩家和“X”怪物MX自己
