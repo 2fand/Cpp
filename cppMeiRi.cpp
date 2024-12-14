5336008789f0f27db1ceb267f7e2763c9b785b61
2fand <167662034+2fand@users.noreply.github.com>
2024-12-14 17:43:57 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -7092,3 +7092,129 @@ int main() {
 	return 0;
 }
 *///也是单向链表的实际应用之一^
+/*
+//MX.cpp
+#include <iostream>
+using namespace std;
+#include <cstring>
+#include "MX.h"
+//怪物MX的设置函数
+void MX::set(vector<trir>* v, char** cpp, int iheal, map<char**, WASD>*, char** pcpp, char(*strmap)[11][11], int, int, bool, bool) {
+	m_v = v;
+	if (nullptr == cpp) {
+		m_cpp = cpp;
+	}
+	else {
+		tempcp = *cpp;
+		cpp = NULL;
+		m_cpp = &tempcp;
+	}
+	m_pcpp = pcpp;
+	nullptr != strmap && (m_strmap = strmap);
+	m_iheal = iheal;
+	setb = 1;
+	
+}
+//怪物MX的额外设置函数
+void MX::exset(char strmapr[11][11]) {
+	memcpy(m_strmapr, strmapr, sizeof m_strmapr);
+}
+//构造函数
+MX::MX() {
+	set();
+}
+void MX::mosterdo() {
+	vector<trir>::iterator it = m_v->begin();
+	if (setb) {
+		pxy = { (*m_cpp - &(*m_strmap)[0][0]) / 11, (*m_cpp - &(*m_strmap)[0][0]) % 11 };//设置怪物MX的坐标
+		m_cc = m_strmapr[pxy.first][setb = 0, pxy.second];//设置盖住的块
+	}
+	for (; m_v->end() != it; it++) {
+		if (*m_cpp == it->cp || *m_cpp == it->cp - (1 - it->b * 2)) {//如果怪物受伤了就停止这个循环
+			break;
+		}
+	}
+	if (m_iheal > 0 && (*m_cpp == *m_pcpp || m_v->end() != it)) {//如果刚才的这个循环停止了，并且还有生命且不在玩家的位置上
+		tempcp = *m_cpp;//防指针的链式移动
+		char* tempcpb = tempcp;
+		bool tempb = 1;//防误覆盖空位
+		m_cpp = NULL;
+		do {
+			tempb && m_v->end() != it && (*tempcp = ' ');//便于移动之后的显示
+			tempcp = &(*m_strmap)[pxy.first = rand() % 9 + 1][pxy.second = rand() % 9 + 1];//怪物MX的移动
+		} while ((tempcp == &(*m_strmap)[9][1] || '@' == (*m_strmap)[pxy.first][pxy.second] || '@' == (*m_strmap)[pxy.first][pxy.second - 1] || '@' == (*m_strmap)[pxy.first][pxy.second + 1] || tempcp == *m_pcpp || '*' == tempcp[-1] || '*' == tempcp[1] || 'X' == (tempcp)[-1] || 'X' == *tempcp || 'X' == (tempcp)[1]) && (tempb = 0, 1));//如果满足上述的条件，就把误覆盖空位的“开关”启动
+		m_cpp = &tempcp;
+		*tempcpb = m_cc;
+		('@' != m_strmapr[pxy.first][pxy.second] || 'X' != m_strmapr[pxy.first][pxy.second] && 'P' != m_strmapr[pxy.first][pxy.second]) && (m_cc = m_strmapr[pxy.first][pxy.second]);//记录盖住的块，不记录“@”子弹，“P”玩家和“X”怪物MX自己
+	}
+	//main函数使见
+}//被玩家碰到传，受伤传(两旁有砖不传)，并见
+char MX::getm() {
+	return 'X';
+}
+char MX::getm(bool) {
+	return m_cc;
+}
+//MO.cpp
+#include <iostream>
+using namespace std;
+#include "MO.h"
+#define NNEG(A, B) ((A) < (B) ? 1 : (A) > (B) ? -1 : 0)//判断宏
+#define MOWAIT 1//怪物MO等待的回合数
+pair<int, int> MO::s_m_pxy = {0, 0};//玩家坐标的初始化
+//构造函数
+MO::MO() {
+	set();
+}
+//MO的设置函数
+void MO::set(vector<trir>* v, char** cpp, int iheal, map<char**, WASD>*m, char**, char(*)[11][11], int ix, int iy, bool, bool) {
+	m_v = v;
+	m_cpp = cpp;
+	m_iheal = iheal;
+	m_pxy.first = ix;
+	m_pxy.second = iy;
+	imove = 0;
+	if (nullptr != m) { 
+		m_m = *m;
+	}
+}
+void MO::mosterdo() {
+    '*' != **m_cpp && (**m_cpp = ' ');//便于移动之后的显示
+	tempcp = *m_cpp;//防指针的链式带动
+	m_cpp = NULL;
+	if (!m_m.empty()) {//沿路模式
+		int arr[4] = { -11, -1, 11, 1 };//怪物MO可能会移动的四个方位
+		for (map<char**, WASD>::iterator it = m_m.begin(); m_m.end() != it; it++) {//改变怪物MO的方向
+			E != it->second && tempcp == *it->first && (m_wasd = it->second);
+		}
+		switch (tempcp += (arr[m_wasd]), m_wasd) {//怪物的移动与xy坐标的变化
+		case W:
+			m_pxy.second--;
+			break;
+		case A:
+			m_pxy.first--;
+			break;
+		case S:
+			m_pxy.second++;
+			break;
+		case D:
+			m_pxy.first++;
+			break;
+		default:
+			break;
+		}
+	}
+	else {//追踪模式
+		MOWAIT == imove % (MOWAIT + 1)/*如果MO等待了MOWAIT回合*/ && ('*' != *(tempcp + 11 * NNEG(m_pxy.first, s_m_pxy.first))/*并且垂直移动的方向上没有墙*/ && (tempcp += 11 * NNEG(m_pxy.first, s_m_pxy.first)/*那么怪物MO就往玩家垂直地移动*/, m_pxy.first += NNEG(m_pxy.first, s_m_pxy.first)/*怪物的x坐标也随之变化*/));//根据怪物的xy坐标和玩家的xy坐标来移动
+		MOWAIT == imove++ % (MOWAIT + 1) && ('*' != *(tempcp + NNEG(m_pxy.second, s_m_pxy.second)/*并且水平移动的方向上没有墙*/) && (tempcp += NNEG(m_pxy.second, s_m_pxy.second)/*那么怪物MO就往玩家水平地移动*/, m_pxy.second += NNEG(m_pxy.second, s_m_pxy.second)/*怪物的y坐标也随之变化*/));
+	}
+	m_cpp = &tempcp;
+}//遇向则转，看路前行（1），或追玩者（2）
+//设置玩家的坐标
+void MO::set_s_pxy(int ix, int iy) {
+	s_m_pxy = { ix, iy };
+}
+char MO::getm() {
+	return 'O';
+}
+*///解决在小BOSS战中有的怪物MO乱走，怪物MX在被子弹打后移动本不应该移动的另一个怪物MX并且也没有使移动前的位置设为空格的两个bug^
