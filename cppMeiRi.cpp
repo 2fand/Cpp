1ff29b3078ad9217ed6e914ff6a5902ac14fab78
2fand <167662034+2fand@users.noreply.github.com>
2024-12-20 19:53:14 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -9820,3 +9820,133 @@ public:
 	}
 }
 *///已实现mybintree二叉树的max方法^
+/*
+//mybintree.hpp
+#pragma once
+#include <iostream>
+#include <queue>
+using namespace std;
+template<class T>
+class mylist {
+private:
+	int ic;
+	class node {
+	public:
+		T t;
+		node* left;
+		node* right;
+		void set() {
+			this->t = NULL;
+			this->left = nullptr;
+			this->right = nullptr;
+		}
+		void set(T tf, node* leftf = nullptr, node* rightf = nullptr) {
+			this->t = tf;
+			this->left = leftf;
+			this->right = rightf;
+		}
+	};
+	node* root;
+public:
+	mylist() {
+		root = new node;
+		root->set();
+		this->ic = 0;
+	}
+	void insert(T t, node** rootf = nullptr) {
+		if (ic) {
+			(*rootf)->t = t;
+		}
+		else if (nullptr != rootf){
+			if (t > (*rootf)->t) {
+				insert(t, &(*rootf)->right);
+			}
+			else {
+				insert(t, &(*rootf)->left);
+			}
+		}
+		else {
+			*rootf = new node;
+			(*rootf)->set(t);
+			ic++;
+		}
+	}
+	int indexfind(T t) {
+		
+	}
+	int capacity() const {
+		return ic;
+	}
+	void operator=(mylist& ml) {
+		int ia = 0;
+		node* findnode = ml.root;
+		for (int i = 0; i < ml.ic; i++) {
+			findnode = findnode->next;
+			this->insert(findnode->t);
+		}
+	}
+	mylist(mylist& ml) {
+		*this = ml;
+	}
+	mylist(const T tarr[], int has) {
+		root = new node;
+		root->set();
+		
+	}
+	void del_back() {
+		if (ic) {
+			if (nullptr == root->right) {
+				root->t = NULL;
+			}
+			else {
+				node** findnode = &root;
+				while (nullptr != (*findnode)->right->right) {
+					findnode = &(*findnode)->right;
+				}
+				node** delnode = &(*findnode)->right;
+				(*findnode)->right = nullptr;
+				delete* delnode;
+			}
+			ic--;
+		}
+	}
+	void clear() {
+		while (ic) {
+			del_back();
+		}
+	}
+	~mylist() {
+		clear();
+		delete root;
+	}
+	void del_index(int index) {
+		
+	}
+	bool IsEmpty() const {
+		return !ic;
+	}
+	T at(int i) {
+		
+	}
+	T operator[](int i) {
+		
+	}
+	void printtree(void (*printfun)(T item, bool b)) {
+		
+	}
+	T max() {
+		node** findnode = &root;
+		while (nullptr != (*findnode)->right) {
+			findnode = &(*findnode)->right;
+		}
+		return (*findnode)->t;
+	}
+	T min() {
+		node** findnode = &root;
+		while (nullptr != (*findnode)->left) {
+			findnode = &(*findnode)->left;
+		}
+		return (*findnode)->t;
+	}
+}
+*///已实现mybintree二叉树的min方法^
