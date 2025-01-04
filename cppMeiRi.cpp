19348df5b98b3b124a607a0c62cbabdc47e54662
2fand <167662034+2fand@users.noreply.github.com>
2025-01-04 18:00:37 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -40503,3 +40503,32 @@ bool graph::islink(int nodea, int nodeb){
 
 }
 *///已构建出mygraph无向图的构造方法及析构方法^
+/*
+//mygraph.cpp
+#include "mygraph.h"
+graph::graph(int inodesf){
+	inodes = inodesf;
+	iedges = 0;
+	adj = new queue<int>[inodes];
+}
+graph::graph(graph& g){
+	inodes = g.inodes;
+	iedges = g.iedges;
+	adj = new queue<int>[inodes];
+}
+graph::~graph(){
+	delete[] adj;
+}
+void graph::link(int nodea, int nodeb){
+
+}
+void graph::dellink(int nodea, int nodeb){
+
+}
+queue<int> graph::getlink(int node){
+
+}
+bool graph::islink(int nodea, int nodeb){
+
+}
+*///已构建出mygraph图中的拷贝构造方法^
