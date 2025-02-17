633b050f8cba2c41605adb4946b8cac1ee28d68c
2fand <167662034+2fand@users.noreply.github.com>
2025-02-17 19:14:17 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -50072,3 +50072,127 @@ int main()
     return 0;
 }
 *///已测试阶进制数类unsignedHierarchicalNum的有参构造方法和toUnsignedInt方法^
+/*
+//HierarchicalNum.hpp
+#include <iostream>
+#include <vector>
+using namespace std;
+class unsignedHierarchicalNum
+{
+private:
+	vector<unsigned int>num;
+	unsigned int len;
+	unsigned int toUnsignedInt(vector<unsigned int>& num) {
+		unsigned int u_i = 0;
+		unsigned int digitNum = 1;
+		auto it = num.end() - 1;
+		while (num.begin() <= it) {
+			u_i += *it * digitNum;
+			digitNum *= digitNum + 1;
+			it--;
+		}
+		return u_i;
+	}
+	void add(const int addNum, vector<unsigned int>& n) {
+		unsigned int upNum = toUnsignedInt(n) + addNum;
+		auto it = n.end() - 1;
+		unsigned int levelUp = 2;
+		while (it <= n.begin()) {
+			*it = upNum % levelUp;
+			upNum /= levelUp;
+			levelUp++;
+			it--;
+			*it = upNum;
+		}
+	}
+	void sub(const int subNum, vector<unsigned int>& n) {
+		unsigned int upNum = toUnsignedInt(n) - subNum;
+		auto it = n.end() - 1;
+		unsigned int levelUp = 2;
+		while (it <= n.begin()) {
+			*it = upNum % levelUp;
+			upNum /= levelUp;
+			levelUp++;
+			it--;
+			*it = upNum;
+		}
+	}
+public:
+	unsignedHierarchicalNum(const unsigned int n, const unsigned int numLen = 0) {
+		if (numLen) {
+			this->len = numLen;
+			this->num.reserve(numLen);
+		}
+		else {
+			unsigned int tim = 1;
+			unsigned int i = 1;
+			while (tim < n) {
+				tim *= ++i;
+			}
+			this->len = i;
+			for (; i; i--) {
+				this->num.push_back(0);
+			}
+		}
+		add(n, this->num);
+	}
+	unsignedHierarchicalNum operator+(const int n) {
+		unsignedHierarchicalNum num(this->toUnsignedInt() + n);
+		return num;
+	}
+	unsignedHierarchicalNum operator-(const int n) {
+		unsignedHierarchicalNum num(this->toUnsignedInt() - n);
+		return num;
+	}
+	unsignedHierarchicalNum& operator++() {
+		add(1, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum& operator--() {
+		sub(1, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum operator++(int) {
+		unsignedHierarchicalNum num = *this;
+		add(1, this->num);
+		return num;
+	}
+	unsignedHierarchicalNum operator--(int) {
+		unsignedHierarchicalNum num = *this;
+		sub(1, this->num);
+		return num;
+	}
+	unsignedHierarchicalNum& operator+=(const int addNum) {
+		add(addNum, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum& operator-=(const int subNum) {
+		sub(subNum, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum& operator=(unsignedHierarchicalNum& n) {
+		this->len = n.len;
+		this->num = n.num;
+		return *this;
+	}
+	unsigned int getPosNum(const unsigned int position) {
+		if (position >= num.size()) {
+			return NULL;
+		}
+		else {
+			return num[position];
+		}
+	}
+	unsigned int toUnsignedInt() {
+		unsigned int u_i = 0;
+		unsigned int digitNum = 1;
+		auto it = this->num.end() - 1;
+		while (this->num.begin() <= it) {
+			u_i += *it * digitNum;
+			digitNum *= digitNum + 1;
+			it--;
+		}
+		return u_i;
+	}
+};
+*///已尝试修正unsignedHierarchicalNum阶进制数类的构造方法^
