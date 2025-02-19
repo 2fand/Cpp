eb1be270129147c0849c7bee6c759db16f5f25b1
2fand <167662034+2fand@users.noreply.github.com>
2025-02-19 18:55:00 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -50333,3 +50333,138 @@ int main()
     return 0;
 }
 *///已测试阶进制数unsignedHierarchicalNum的加号重载运算符^
+/*
+//HierarchicalNum.hpp
+#include <iostream>
+#include <vector>
+using namespace std;
+class unsignedHierarchicalNum
+{
+private:
+	vector<unsigned int>num;
+	unsigned int len;
+	unsigned int toUnsignedInt(vector<unsigned int>& num) {
+		unsigned int u_i = 0;
+		unsigned int digitNum = 1;
+		unsigned int timNum = 2;
+		int it = num.size() - 1;
+		while (0 <= it) {
+			u_i += num[it--] * digitNum;
+			digitNum *= timNum++;
+		}
+		return u_i;
+	}
+	void add(const int addNum, vector<unsigned int>& n) {
+		unsigned int upNum = toUnsignedInt(n) + addNum;
+		int it = n.size() - 1;
+		unsigned int levelUp = 2;
+		while (it >= 0) {
+			n[it] = upNum % levelUp;
+			upNum /= levelUp;
+			levelUp++;
+			it-- ? n[it] = upNum : 0;
+		}
+	}
+	void sub(const int subNum, vector<unsigned int>& n) {
+		unsigned int upNum = toUnsignedInt(n) - subNum;
+		int it = n.size() - 1;
+		unsigned int levelUp = 2;
+		while (it >= 0) {
+			n[it] = upNum % levelUp;
+			upNum /= levelUp;
+			levelUp++;
+			it-- ? n[it] = upNum : 0;
+		}
+	}
+public:
+	unsignedHierarchicalNum(const unsigned int n, const unsigned int numLen = 0) {
+		unsigned int tim = 1;
+		unsigned int i = 1;
+		while (!numLen && tim < n) {
+			tim *= ++i;
+		}
+		this->len = i;
+		for (; i; i--) {
+			this->num.push_back(0);
+		}
+		add(n, this->num);
+	}
+	unsignedHierarchicalNum operator+(const int n) {
+		unsignedHierarchicalNum num(this->toUnsignedInt() + n);
+		return num;
+	}
+	unsignedHierarchicalNum operator-(const int n) {
+		unsignedHierarchicalNum num(this->toUnsignedInt() - n);
+		return num;
+	}
+	unsignedHierarchicalNum& operator++() {
+		add(1, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum& operator--() {
+		sub(1, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum operator++(int) {
+		unsignedHierarchicalNum num = *this;
+		add(1, this->num);
+		return num;
+	}
+	unsignedHierarchicalNum operator--(int) {
+		unsignedHierarchicalNum num = *this;
+		sub(1, this->num);
+		return num;
+	}
+	unsignedHierarchicalNum& operator+=(const int addNum) {
+		add(addNum, this->num);
+		return *this;
+	}
+	unsignedHierarchicalNum& operator-=(const int subNum) {
+		sub(subNum, this->num);
+		return *this;
+	}
+	unsigned int operator=(unsigned int n) {
+		unsigned int upNum = n;
+		int it = this->num.size() - 1;
+		unsigned int levelUp = 2;
+		while (it >= 0) {
+			this->num[it] = upNum % levelUp;
+			upNum /= levelUp;
+			levelUp++;
+			it-- ? this->num[it] = upNum : 0;
+		}
+		return n;
+	}
+	unsignedHierarchicalNum& operator=(unsignedHierarchicalNum& n) {
+		this->len = n.len;
+		this->num = n.num;
+		return *this;
+	}
+	unsigned int getPosNum(const unsigned int position) const {
+		return position >= num.size() ? NULL : num[position];
+	}
+	unsigned int toUnsignedInt() {
+		unsigned int u_i = 0;
+		unsigned int digitNum = 1;
+		unsigned int timNum = 2;
+		int it = this->num.size() - 1;
+		while (0 <= it) {
+			u_i += this->num[it--] * digitNum;
+			digitNum *= timNum++;
+		}
+		return u_i;
+	}
+};
+//meiri.cpp
+#include<iostream>
+#include "HierarchicalNum.hpp"
+using namespace std;
+
+int main()
+{
+    unsignedHierarchicalNum u_n(100);
+    u_n = 10;
+    cout << u_n.toUnsignedInt();
+    return 0;
+}
+*///已新建并测试了阶进制数类unsignedHierarchicalNum的返回无符号整数的重载赋值运算符^
