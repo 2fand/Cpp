b1bbd48b11ac0266b1f825af7445854d7983572d
2fand <167662034+2fand@users.noreply.github.com>
2025-02-25 19:49:31 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -50916,3 +50916,58 @@ public:
 	}
 };
 *///已修正Sequence类的pushBackItem方法^
+/*
+//Sequence.hpp
+#include <iostream>
+#include <vector>
+#include "HierarchicalNum.h"
+using namespace std;
+template<class T>
+class Sequence {
+private:
+	vector<T>items;
+	vector<vector<T>>sequenceVector;
+	unsigned int maxNum;
+	unsigned int swapNum;
+public:
+	Sequence() {
+		this->maxNum = 1;
+		this->swapNum = 0;
+	}
+	Sequence(const unsigned int len) {
+		this->maxNum = 1;
+		this->swapNum = 0;
+		unsigned int factNum = len - 1;
+		while (this->maxNum *= factNum, factNum-- > 1){}
+		while (len--) { this->items.push_back(0); }
+	}
+	Sequence(const vector<T> f_items) {
+		this->items = f_items;
+		this->maxNum = 1;
+		this->swapNum = 0;
+		unsigned int factNum = this->items.size() - 1;
+		while (this->maxNum *= factNum, factNum-- > 1){}
+	}
+	void swapVectorItem(const unsigned int index, const unsigned int indexa) {
+		T tempItem = this->items[index];
+		this->items[index] = this->items[indexa];
+		this->items[indexa] = tempItem;
+	}
+	vector<vector<T>>getSequence() {
+
+	}
+	unsigned int sequenceCount() {
+		return this->sequenceVector.size();
+	}
+	T pushBackItem(const T item) {
+		this->items.push_back(item);
+		this->maxNum *= this->items.size();
+	}
+	T popBackItem(const T item) {
+		if (this->items.size()) {
+			this->maxNum /= this->items.size();
+			this->items.pop_back(item);
+		}
+	}
+};
+*///已重新修正pushBackItem方法，并实现了popBackItem方法^
