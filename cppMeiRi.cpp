c71df05ad01f0b492f936ecd351299156d64b086
2fand <167662034+2fand@users.noreply.github.com>
2025-03-04 18:33:29 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -51641,3 +51641,32 @@ string stringPlus::calc(string str, string stra) {
 	return this->last;
 }
 *///已构建出stringPlus类的基础^
+/*
+//stringCalc.cpp
+#include "stringPlus.h"
+string stringPlus::getLast() {
+	return this->last;
+}
+string stringPlus::calc(string str, string stra) {
+	this->last.clear();
+	bool isAddOne = false;
+	bool itIsZero = str.size();
+	bool itaIsZero = stra.size();
+	auto it = str.crbegin();
+	auto ita = stra.crbegin();
+	int digitNum = 0;
+	while (!itaIsZero || !itIsZero) {
+		digitNum = (itIsZero ? 0 : *it++) + (itaIsZero ? 0 : *ita++) + isAddOne;
+		isAddOne = digitNum / 10;
+		digitNum %= 10;
+		this->last.insert(this->last.begin(), '0' + digitNum);
+		if (str.crend() == it) {
+			itIsZero = true;
+		}
+		if (stra.crend() == ita) {
+			itaIsZero = true;
+		}
+	}
+	return this->last;
+}
+*///已实现stringPlus类的calc方法^
