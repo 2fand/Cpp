7f1e7ed678d74b40be20793096dfdae20acb15e9
2fand <167662034+2fand@users.noreply.github.com>
2025-03-06 19:33:41 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -51766,3 +51766,46 @@ int main() {
     return 0;
 }
 *///已测试stringPlus类的空字符串计算与大值计算^
+/*
+//stringCalc.cpp
+#include "stringPlus.h"
+#include <cmath>
+string stringPlus::getLast() {
+	return this->last;
+}
+string stringPlus::calc(string str, string stra) {
+	this->last.clear();
+	int addNum = 0;
+	bool itIsZero = !str.size();
+	bool itaIsZero = !stra.size();
+	bool strIsNegative = (str.size() ? '-' == str.front() : false);
+	bool straIsNegative = (stra.size() ? '-' == stra.front() : false);
+	bool isNegative = false;
+	auto it = str.crbegin();
+	auto ita = stra.crbegin();
+	int digitNum = 0;
+	while (!itaIsZero || !itIsZero || addNum) {
+		if (str.crend() == it || '0' > *it || '9' < *it) {
+			itIsZero = true;
+		}
+		if (stra.crend() == ita || '0' > *ita || '9' < *ita) {
+			itaIsZero = true;
+		}
+		digitNum = (itIsZero ? 0 : (1 - strIsNegative * 2) * (*it - '0')) + (itaIsZero ? 0 : (1 - straIsNegative * 2) * (*ita - '0')) + addNum;
+		isNegative = digitNum ? digitNum < 0 : isNegative;
+		addNum = (1 - 2 * (itIsZero || str.crend() == it ? 0 : 1 - 2 * strIsNegative * *it) < (itaIsZero || stra.crend() == ita ? 0 : 1 - 2 * straIsNegative * *ita)) * digitNum / 10;
+		digitNum = (itIsZero || str.crend() == it ? 0 : 1 - 2 * strIsNegative * *it) < (itaIsZero || stra.crend() == ita ? 0 : 1 - 2 * straIsNegative * *ita) ? 10 + digitNum : abs(digitNum %= 10);
+		if (!itaIsZero || !itIsZero || addNum) {
+			this->last.insert(this->last.begin(), '0' + digitNum);
+		}
+		(itIsZero || (it++, 0)), (itaIsZero || (ita++, 0));
+	}
+	if (this->last.empty()) {
+		this->last.push_back('0');
+	}
+	else if (isNegative) {
+		this->last.insert(this->last.begin(), '-');
+	}
+	return this->last;
+}
+*///calc方法就先改到这边^
