91f4a984c69d3211a7747b6f9f2619f824b554c8
2fand <167662034+2fand@users.noreply.github.com>
2025-03-20 19:24:16 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -53348,3 +53348,34 @@ string stringSub::sub(string str, string stra) {
 	return this->last;
 }
 *///已修正stringSub类的sub方法中stra转负数的操作^
+/*
+//stringTim.cpp
+#include "stringTim.h"
+string stringTim::tim(string str, string stra) {
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	bool isNegative = 1 == ('-' == str.front()) + ('-' == stra.front()) && (atoi(str.c_str()) && atoi(stra.c_str()));
+	int i = abs(atoi(stra.c_str()));
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+		if ('-' == timStr[0] && '-' == timStr[1]) {
+			for (int i = 0; i < timStr.size() - 2; i++) {
+				timStr[i] = timStr[i + 2];
+			}
+			timStr.erase(timStr.size() - 2);
+		}
+	}
+	this->last = timStr;
+	return timStr;
+}
+*///stringTin类的tim方法已基本无bug^
