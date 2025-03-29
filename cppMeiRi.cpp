3c08c73b500e126eb0cedc31dc8faaa3cfe5f9a9
2fand <167662034+2fand@users.noreply.github.com>
2025-03-29 17:18:42 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -54485,3 +54485,54 @@ string stringMod::getLast() {
 	return this->last;
 }
 *///已更改stringMod类中的mod计算方法的实现^
+/*
+//stringTim.cpp
+#include "stringTim.h"
+string stringTim::tim(string str, string stra) {
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	bool isNegative = 1 == ('-' == str.front()) + ('-' == stra.front()) && (atoi(str.c_str()) && atoi(stra.c_str()));
+	int i = abs(atoi(stra.c_str()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+	}
+	this->last = timStr;
+	return timStr;
+}
+string stringTim::timAssign(string& str, string stra) {
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	bool isNegative = 1 == ('-' == str.front()) + ('-' == stra.front()) && (atoi(str.c_str()) && atoi(stra.c_str()));
+	int i = abs(atoi(stra.c_str()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+	}
+	str = this->last = timStr;
+	return timStr;
+}
+string stringTim::getLast() {
+	return this->last;
+}
+*///已修改stringTim类的tim计算方法^
