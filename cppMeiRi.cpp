7751b5eef74352aec1133859f4f9afa9a1395191
2fand <167662034+2fand@users.noreply.github.com>
2025-04-03 17:56:40 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -54948,3 +54948,144 @@ int main() {
     return 0;
 }
 *///测试stringPow类的pow方法^
+/*
+//stringDiv.cpp
+#include "stringDiv.h"
+string stringDiv::div(string str, string stra) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	bool isNegative = (1 == ('-' == str.front()) + ('-' == stra.front()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	'-' == stra.front() ? stra = sub("0", stra) : "";
+	if ("0" == stra) {
+		cout << "错误：不能除以0" << endl;
+		exit(1);
+	}
+	string divStr = "0";
+	while (atoi(sub(str, stra).c_str()) >= 0) {
+		str = sub(str, stra);
+		divStr = sub(divStr, "-1");
+	}
+	if (isNegative && "0" != divStr) {
+		divStr.insert(0, "-");
+	}
+	this->last = divStr;
+	return divStr;
+}
+string stringDiv::divAssign(string& str, string stra) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	bool isNegative = (1 == ('-' == str.front()) + ('-' == stra.front()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	'-' == stra.front() ? stra = sub("0", stra) : "";
+	if ("0" == stra) {
+		cout << "错误：不能除以0" << endl;
+		exit(1);
+	}
+	string divStr = "0";
+	while (atoi(sub(str, stra).c_str()) >= 0) {
+		str = sub(str, stra);
+		divStr = sub(divStr, "-1");
+	}
+	if (isNegative && "0" != divStr) {
+		divStr.insert(0, "-");
+	}
+	str = this->last = divStr;
+	return divStr;
+}
+string stringDiv::getLast() {
+	return this->last;
+}
+string stringDiv::div(string str, string stra, bool isUp){
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	bool isNegative = (1 == ('-' == str.front()) + ('-' == stra.front()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	'-' == stra.front() ? stra = sub("0", stra) : "";
+	if ("0" == stra) {
+		cout << "错误：不能除以0" << endl;
+		exit(1);
+	}
+	string divStr = "0";
+	while (atoi(sub(str, stra).c_str()) >= 0) {
+		str = sub(str, stra);
+		divStr = sub(divStr, "-1");
+	}
+	1 == isNegative + isUp && "0" != str ? subAssign(divStr, "-1") : "";
+	if (isNegative && "0" != divStr) {
+		divStr.insert(0, "-");
+	}
+	this->last = divStr;
+	return divStr;
+}
+string stringDiv::divAssign(string& str, string stra, bool isUp) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	bool isNegative = (1 == ('-' == str.front()) + ('-' == stra.front()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	'-' == stra.front() ? stra = sub("0", stra) : "";
+	if ("0" == stra) {
+		cout << "错误：不能除以0" << endl;
+		exit(1);
+	}
+	string divStr = "0";
+	while (atoi(sub(str, stra).c_str()) >= 0) {
+		str = sub(str, stra);
+		divStr = sub(divStr, "-1");
+	}
+	1 == isNegative + isUp && "0" != str ? subAssign(divStr, "-1") : "";
+	if (isNegative && "0" != divStr) {
+		divStr.insert(0, "-");
+	}
+	str = this->last = divStr;
+	return divStr;
+}
+*///已修正stringDiv类的div及一系列计算方法^
