482fd7969c7f0b50b8ad0a3268e2e53df5caaace
2fand <167662034+2fand@users.noreply.github.com>
2025-04-06 17:38:03 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -55267,3 +55267,86 @@ int main() {
     return 0;
 }
 *///测试stringTimDecimal类的timDecimal方法^
+/*
+//stringTimDecimal.cpp
+#include "stringTimDecimal.h"
+string stringTimDecimal::getLast() {
+	return this->last;
+}
+string stringTimDecimal::timDecimal(string str, string stra) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && '.' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && '.' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	!str.size() ? str = "0" : "";
+	!stra.size() ? stra = "0" : "";
+	string strDigit = "";
+	string straDigit = "";
+	for (char ch : str) {
+		if ('.' != ch) {
+			strDigit.push_back(ch);
+		}
+	}
+	for (char ch : stra) {
+		if ('.' != ch) {
+			straDigit.push_back(ch);
+		}
+	}
+	regex dotRegex("\\.");
+	bool hasDot = regex_search(str, dotRegex);
+	bool hasDota = regex_search(stra, dotRegex);
+	int dotBackNum = hasDot * (str.size() - 1 - str.find('.')) + hasDota * (stra.size() - 1 - stra.find('.'));
+	bool canDelete = true;
+	string timStr = tim(straDigit, strDigit);
+	while (timStr.size() <= dotBackNum) {
+		timStr.insert(0, "0");
+	}
+	int dotIndex = timStr.size() - dotBackNum;
+	regex correntNumRegex("\\.0*$");
+	timStr.insert(dotIndex, ".");
+	timStr = regex_replace(timStr, correntNumRegex, "");
+	return this->last = timStr;
+}
+string stringTimDecimal::timDecimalAssign(string& str, string stra) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && '.' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && '.' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	!str.size() ? str = "0" : "";
+	!stra.size() ? stra = "0" : "";
+	string strDigit = "";
+	string straDigit = "";
+	for (char ch : str) {
+		if ('.' != ch) {
+			strDigit.push_back(ch);
+		}
+	}
+	for (char ch : stra) {
+		if ('.' != ch) {
+			straDigit.push_back(ch);
+		}
+	}
+	regex dotRegex("\\.");
+	bool hasDot = regex_search(str, dotRegex);
+	bool hasDota = regex_search(stra, dotRegex);
+	int dotBackNum = hasDot * (str.size() - 1 - str.find('.')) + hasDota * (stra.size() - 1 - stra.find('.'));
+	bool canDelete = true;
+	string timStr = tim(straDigit, strDigit);
+	while (timStr.size() <= dotBackNum) {
+		timStr.insert(0, "0");
+	}
+	int dotIndex = timStr.size() - dotBackNum;
+	regex correntNumRegex("\\.0*$");
+	timStr.insert(dotIndex, ".");
+	timStr = regex_replace(timStr, correntNumRegex, "");
+	return this->last = str = timStr;
+}
+*///已修正timDecimal方法^
