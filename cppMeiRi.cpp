d8b2cc0e30352c0e4eab544367402870d5a4c28e
2fand <167662034+2fand@users.noreply.github.com>
2025-04-07 18:38:54 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -55371,3 +55371,186 @@ int main() {
     return 0;
 }
 *///再测试stringTimDecimal类的timDecimal方法^
+/*
+//stringPlus.cpp
+#include "stringPlus.h"
+#include <cmath>
+string stringPlus::getLast() {
+	return this->last;
+}
+string stringPlus::add(string str, string stra) {
+	this->last.clear();
+	int addNum = 0;
+	int ix = 0;
+	int iy = 0;
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (abs(atoi(stra.c_str())) > abs(atoi(str.c_str()))) {
+		str.swap(stra);
+	}
+	bool itIsZero = !str.size();
+	bool itaIsZero = !stra.size();
+	bool strIsNegative = (str.size() ? '-' == str.front() : false);
+	bool straIsNegative = (stra.size() ? '-' == stra.front() : false);
+	bool tenSub = 1 == strIsNegative + straIsNegative;
+	int absStr = abs(atoi(str.c_str()));
+	int absStra = abs(atoi(stra.c_str()));
+	bool isNegative = '-' == (absStr > absStra ? str.front() : absStr == absStra ? false : stra.front());
+	auto it = str.crbegin();
+	auto ita = stra.crbegin();
+	int digitNum = 0;
+	regex delRegex("^0+");
+	while (!itaIsZero || !itIsZero || addNum) {
+		if (str.crend() == it || '0' > *it || '9' < *it) {
+			itIsZero = true;
+		}
+		if (stra.crend() == ita || '0' > *ita || '9' < *ita) {
+			itaIsZero = true;
+		}
+		ix = (1 - 2 * strIsNegative) * (itIsZero ? 0 : *it++ - '0');
+		iy = (1 - 2 * straIsNegative) * (itaIsZero ? 0 : *ita++ - '0');
+		digitNum = (tenSub && abs(ix) < abs(iy)) ? 10 + abs(ix) - abs(iy) : ix + iy;
+		digitNum += !(0 > addNum && 2 == itIsZero + itaIsZero) * (1 - 2 * (digitNum < 0)) * addNum;
+		addNum = tenSub && abs(ix) < abs(iy) ? -1 : digitNum / 10;
+		digitNum = abs(digitNum % 10);
+		if (!itIsZero || !itaIsZero || addNum || digitNum) {
+			this->last.insert(this->last.begin(), '0' + digitNum);
+		}
+	}
+	this->last = regex_replace(this->last, delRegex, "");
+	if (this->last.empty()) {
+		this->last.push_back('0');
+	}
+	else if (isNegative) {
+		this->last.insert(this->last.begin(), '-');
+	}
+	return this->last;
+}
+string stringPlus::addAssign(string& str, string stra) {
+	this->last.clear();
+	int addNum = 0;
+	int ix = 0;
+	int iy = 0;
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (abs(atoi(stra.c_str())) > abs(atoi(str.c_str()))) {
+		str.swap(stra);
+	}
+	bool itIsZero = !str.size();
+	bool itaIsZero = !stra.size();
+	bool strIsNegative = (str.size() ? '-' == str.front() : false);
+	bool straIsNegative = (stra.size() ? '-' == stra.front() : false);
+	bool tenSub = 1 == strIsNegative + straIsNegative;
+	int absStr = abs(atoi(str.c_str()));
+	int absStra = abs(atoi(stra.c_str()));
+	bool isNegative = '-' == (absStr > absStra ? str.front() : absStr == absStra ? false : stra.front());
+	auto it = str.crbegin();
+	auto ita = stra.crbegin();
+	int digitNum = 0;
+	regex delRegex("^0+");
+	while (!itaIsZero || !itIsZero || addNum) {
+		if (str.crend() == it || '0' > *it || '9' < *it) {
+			itIsZero = true;
+		}
+		if (stra.crend() == ita || '0' > *ita || '9' < *ita) {
+			itaIsZero = true;
+		}
+		ix = (1 - 2 * strIsNegative) * (itIsZero ? 0 : *it++ - '0');
+		iy = (1 - 2 * straIsNegative) * (itaIsZero ? 0 : *ita++ - '0');
+		digitNum = (tenSub && abs(ix) < abs(iy)) ? 10 + abs(ix) - abs(iy) : ix + iy;
+		digitNum += !(0 > addNum && 2 == itIsZero + itaIsZero) * (1 - 2 * (digitNum < 0)) * addNum;
+		addNum = tenSub && abs(ix) < abs(iy) ? -1 : digitNum / 10;
+		digitNum = abs(digitNum % 10);
+		if (!itIsZero || !itaIsZero || addNum || digitNum) {
+			this->last.insert(this->last.begin(), '0' + digitNum);
+		}
+	}
+	this->last = regex_replace(this->last, delRegex, "");
+	if (this->last.empty()) {
+		this->last.push_back('0');
+	}
+	else if (isNegative) {
+		this->last.insert(this->last.begin(), '-');
+	}
+	return str = this->last;
+}
+//stringTim.cpp
+#include "stringTim.h"
+string stringTim::tim(string str, string stra) {
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	if (abs(atoi(str.c_str())) < abs(atoi(stra.c_str()))) {
+		str.swap(stra);
+	}
+	bool isNegative = 1 == ('-' == str.front()) + ('-' == stra.front()) && (atoi(str.c_str()) && atoi(stra.c_str()));
+	int i = abs(atoi(stra.c_str()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+	}
+	this->last = timStr;
+	return timStr;
+}
+string stringTim::timAssign(string& str, string stra) {
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	if (abs(atoi(str.c_str())) < abs(atoi(stra.c_str()))) {
+		str.swap(stra);
+	}
+	bool isNegative = 1 == ('-' == str.front()) + ('-' == stra.front()) && (atoi(str.c_str()) && atoi(stra.c_str()));
+	int i = abs(atoi(stra.c_str()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+	}
+	str = this->last = timStr;
+	return timStr;
+}
+string stringTim::getLast() {
+	return this->last;
+}
+*///已优化stringTim类的tim计算方法，并修正stringPlus类中add方法中的有多余0的处理错误^
