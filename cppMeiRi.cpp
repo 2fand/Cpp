b7e8f7c04110cd2e9d3c6bfd9f9d4df46c4874f8
2fand <167662034+2fand@users.noreply.github.com>
2025-04-19 17:51:14 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -57568,3 +57568,66 @@ string stringDivDecimal::divDecimalAssign(string& str, string stra, size_t maxDe
 	return this->last = str = DivStr;
 }
 *///已修正stringDivDeciaml类负数运算错误bug^
+/*
+//stringPow.cpp
+#include "stringPow.h"
+string stringPow::getLast() {
+	return this->last;
+}
+string stringPow::pow(string str, string stra) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && ('-' != str[index] && ('0' > str[index] || '9' < str[index]) || index > 0 && '-' == str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && ('-' != stra[index] && ('0' > stra[index] || '9' < stra[index]) || index > 0 && '-' == stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	int i = atoi(stra.c_str());
+	string powStr = i < 0 && '-' == str.front() ? "-1" : "1";
+	i < 0 && '-' == str.front() ? (str = sub("0", str)) : "";
+	for (; i; i < 0 ? i++ : i--) {
+		if (i < 0) {
+			divDecimalAssign(powStr, str);
+		}
+		else {
+			timDecimalAssign(powStr, str);
+		}
+	}
+	return this->last = powStr;
+}
+string stringPow::powAssign(string& str, string stra) {
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && ('-' != str[index] && ('0' > str[index] || '9' < str[index]) || index > 0 && '-' == str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && ('-' != stra[index] && ('0' > stra[index] || '9' < stra[index]) || index > 0 && '-' == stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	int i = atoi(stra.c_str());
+	string powStr = i < 0 && '-' == str.front() ? "-1" : "1";
+	i < 0 && '-' == str.front() ? (str = sub("0", str)) : "";
+	for (; i; i < 0 ? i++ : i--) {
+		if (i < 0) {
+			divDecimalAssign(powStr, str);
+		}
+		else {
+			timDecimalAssign(powStr, str);
+		}
+	}
+	return this->last = str = powStr;
+}
+*///已支持stringPow类方法小数的运算^
