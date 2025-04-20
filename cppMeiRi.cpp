06dc1688b2acb006207976e5ef6c19ece8d4abe4
2fand <167662034+2fand@users.noreply.github.com>
2025-04-20 17:18:48 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -57631,3 +57631,20 @@ string stringPow::powAssign(string& str, string stra) {
 	return this->last = str = powStr;
 }
 *///已支持stringPow类方法小数的运算^
+/*
+//stringPowDecimal.cpp
+#define _CRT_SECURE_NO_WARNINGS true
+#include "stringPowDecimal.h"
+string stringPowDecimal::getLast() {
+	return this->last;
+}
+string stringPowDecimal::powDecimal(string str, string stra) {
+	char s[10000] = "";
+	sprintf(s, "%lf", pow(strtod(str.c_str(), nullptr), strtod(stra.c_str(), nullptr)));
+	regex r("\\.?0+$");
+	return regex_replace(string(s), r, "");
+}
+string stringPowDecimal::powDecimalAssign(string& str, string stra) {
+	return this->last = str = powDecimal(str, stra);
+}
+*///已实现stringPowDecimal类的方法^
