316dd0b00342e2d9bce59f1382b83fbe92dcbe8d
2fand <167662034+2fand@users.noreply.github.com>
2025-05-04 18:36:43 +0800

    Update cppMeiRi.cpp

--- cppMeiRi.cpp
+++ cppMeiRi.cpp
@@ -58373,3 +58373,30 @@ int main() {
     return 0;
 }
 *///删除偶数位的字符^
+/*
+#include <iostream>
+#include "stringPlus.h"
+#include "stringSub.h"
+#include "stringTim.h"
+#include "stringDiv.h"
+#include "stringMod.h"
+#include "stringPow.h"
+#include "stringTimDecimal.h"
+#include "stringPlusDecimal.h"
+#include "stringSubDecimal.h"
+#include "stringDivDecimal.h"
+#include "stringPowDecimal.h"
+#include <set>
+#include <array>
+#include <list>
+using namespace std;
+int main() {
+    string str = "aaabbbccddeeff";
+    int i = 2;
+    for (; i < str.size(); i += 2) {
+        str.erase(i, 1);
+    }
+    cout << str;
+    return 0;
+}
+*///删除位数为3的倍数的字符^
